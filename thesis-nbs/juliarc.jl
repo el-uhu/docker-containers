6d10d718fcd15b7b1233757c9d9b4cf83b9d3c4e
el-uhu <luki.hutter@gmail.com>
2016-09-20 12:31:54 +0100

    add xppaut

--- thesis-nbs/juliarc.jl
+++ thesis-nbs/juliarc.jl
@@ -1,2 +1,3 @@
 push!(LOAD_PATH, "/home/nbuser/XPPjl")
 push!(LOAD_PATH, "/home/nbuser/PPSIM")
+push!(LOAD_PATH, "/home/nbuser/IDAT/src")
--- thesis-nbs/precompile_key_packages.jl
+++ thesis-nbs/precompile_key_packages.jl
@@ -1,5 +1,7 @@
 using XPP
 using DataFrames
 using DataStructures
+using ExcelReaders
+using WoodburyMatrices
 using Optim
 using YAML
